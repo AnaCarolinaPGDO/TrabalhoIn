91d4e5a9cbc49ea9f021e9fa295d31a53e307ef1
Ana Carolina <50124714+AnaCarolinaPGDO@users.noreply.github.com>
2024-10-31 17:51:55 -0300

    yujjh

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { Exercicio1 } from "./Exercicios/exercicio1"
+// import { AulaUseMemo } from "./pages/Aula-useMemo"
+import { Login } from './Pages/Login'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <AulaUseMemo />
   )
 }
 
-export default App
+// useEffect
+// Imutabilidade
+// Componente de Decisao
+// Rotas
+// Api axios
+// {Exercicio1 /}
+// {Exercicio2 /}
+// {Exercicio3 /}
+// {Exercicio4 /}
+// {Exericico5 /}
+{ Login }
+
--- src/Pages/Login/style.jsx
+++ src/Pages/Login/style.jsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-
 export const Container = styled.div`
   background-color: #838383;
   display: flex;
